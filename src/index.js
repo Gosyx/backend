e82360fbac0e1b006d90deea752ff48558bc63ad
Gustavo Ortiz <ggo9945@gmail.com>
2023-07-23 17:04:55 -0300

    websocket

--- src/index.js
+++ src/index.js
@@ -1,44 +1,60 @@
 import express from "express";
 import { engine } from "express-handlebars";
-import __dirname from "./utils.js";
+import { __dirname } from "./utils.js";
 import * as path from "path";
-import ProductRouter from "./router/products.routes.js";
-import CartRouter from "./router/carts.routes.js";
-import ProductManager from "./controllers/ProductManager.js";
+import { Server } from "socket.io";
 
 const app = express();
 const PORT = 8080;
-const product = new ProductManager();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const server = app.listen(PORT, () => {
+  console.log(`Server run Express port: ${PORT}`);
+});
 
-app.use("/api/products", ProductRouter);
-app.use("/api/cart", CartRouter);
+const io = new Server(server);
 
-//estructura handlebars
-app.engine("handlebars", engine());
+app.engine("handlebars", engine({ layoutsDir: path.resolve(__dirname, "views/layouts") }));
 app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname + "/views"));
 
-//archivo estatico
 app.use("/", express.static(__dirname + "/public"));
 
-app.get("/", async (req, res) => {
-  let allProducts = await product.getProducts();
-  res.render("home", {
-    title: "Hanasita | Handlebars",
-    products: allProducts,
-  });
+app.get("/", (req, res) => {
+  res.render("index");
 });
-app.get("/:id", async (req, res) => {
-  console.log(req.params);
-  let prod = await product.getProductsById(req.params.id);
-  res.render("prod", {
-    title: "Hanasita | Handlebars",
-    products: prod,
+
+const message = [];
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} Connection`);
+
+  //Nombre del usuario
+  let userName = "";
+  // Mesaje de Coneccion
+  socket.on("userConnection", (data) => {
+    userName = data.user;
+    message.push({
+      id: socket.id,
+      info: "connection",
+      name: data.user,
+      message: `${data.user} Connectado`,
+      date: new Date().toTimeString(),
+    });
+    io.sockets.emit("userConnection", message);
   });
-});
-app.listen(PORT, () => {
-  console.log(`Servidor por puerto ${PORT}`);
+  // Mensaje de Mesaje enviado
+  socket.on("userMessage", (data) => {
+    message.push({
+      id: socket.id,
+      info: "message",
+      name: userName,
+      message: data.message,
+      date: new Date().toTimeString(),
+    });
+    io.sockets.emit("userMessage", message);
+  });
+  //Mensage Usuario escribiendo
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
 });
